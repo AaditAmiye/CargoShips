997d847eeead15ad7a7cce85b3448fcfd09af3ed
Aadit Amiye <108892929+AaditAmiye@users.noreply.github.com>
2026-06-19 01:17:31 +0530

    new

--- src/compnents/msc/msc.jsx
+++ src/compnents/msc/msc.jsx
@@ -1,6 +1,6 @@
 // import React from 'react'
 import './msc.css'
-import msc_ship from '../../assets/make_this_for_CMA_CGM_202606180018.jpeg'
+import msc_ship from '../../assets/cma_ship.jpeg'
 const msc = () => {
   return (
    <div className="msc">
