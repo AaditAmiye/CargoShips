5caaf792cee17cf73301ac07fd1e272ff9ec171b
Aadit Amiye <108892929+AaditAmiye@users.noreply.github.com>
2026-06-23 22:40:15 +0530

    Initial commit

---
+++ src/compnents/msc/msc.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+import './msc.css'
+import msc_ship from '../../assets/cma_ship.jpeg'
+const msc = () => {
+  return (
+   <div className="msc">
+  <img  src={msc_ship} />
+  
+</div>
+  )
+}
+
+export default msc
